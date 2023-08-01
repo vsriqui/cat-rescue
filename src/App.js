e0483db3289c4d8a6d7248175c28bd82c98fb6b9
Victor Sriqui <victorsriqui@gmail.com>
2023-08-01 15:49:54 -0600

    initial: Commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  console.log('Initial commit')
+
   return (
     <div className="App">
       <header className="App-header">

f731cebbe9976cca9cd5e545aa6ba70c01686c6d
Victor Sriqui <victorsriqui@gmail.com>
2023-08-02 16:11:50 -0600

    feat: Api calls with malformed data checking.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import {getCats, malformedCats, mapCats} from './apiCalls-Functions'
 
 function App() {
 
+  const [cats, setCats] = useState([]);
 
-  console.log('Initial commit')
+  // ''
+
+  useEffect(() => {
+    const fetchCats = () => {
+      getCats('search?limit=10&has_breeds=1&order=RAND&api_key=sNw9kk1ppt2nofxJ8GLOdkJzKycbk6iOmsQsydl8v4rRmaPkXomMGWamZwmhkH4y')
+        .then((bigMeow) => Promise.all(mapCats(bigMeow)))
+        .then((catData) => malformedCats(catData))
+        .then((catData) => setCats(catData))
+        .catch((hisss) => alert(hisss));
+    };
+    fetchCats();
+  }, [])
+  
+
+console.log(cats, 'dat')
+
+
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reloa.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
